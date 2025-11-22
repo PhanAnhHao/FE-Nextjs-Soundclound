315de182e1b046f61cce94ea4b513379cc0f8742
Anh Hào Phan <pdkenny25@gmail.com>
2025-11-22 17:24:02 +0700

    Module HomePage: Fetch Wrapper (Javascript)

--- nextjs-mui-ts-client/src/app/page.tsx
+++ nextjs-mui-ts-client/src/app/page.tsx
@@ -1,22 +1,32 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
+import { sendRequestJS } from "@/utils/old.api";
 
 export default async function HomePage() {
 
-  const res = await fetch(`http://localhost:8000/api/v1/tracks/top`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        category: 'CHILL',
-        limit: 10
-      }),
-    }
-  );
+  // const res = await fetch(`http://localhost:8000/api/v1/tracks/top`,
+  //   {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       categories: 'CHILL',
+  //       limit: 10
+  //     }),
+  //   }
+  // );
+  // console.log("Fetch response server at home page:", await res.json());
 
-  console.log("Fetch response server at home page:", await res.json());
+  const res = await sendRequestJS({
+    url: `http://localhost:8000/api/v1/tracks/top`,
+    method: 'POST',
+    body: {
+      category: 'CHILL',
+      limit: 2
+    }
+  });
+  console.log("API JS response server at home page:", res);
 
   return (
     <Container>
