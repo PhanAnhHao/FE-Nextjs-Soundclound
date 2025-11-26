51ea9b62da176dad845ca8cc75120bdeca91ff9f
Anh Hào Phan <pdkenny25@gmail.com>
2025-11-26 12:47:33 +0700

    Module Wavesurfer: Search Params

--- nextjs-mui-ts-client/src/app/track/[slug]/page.tsx
+++ nextjs-mui-ts-client/src/app/track/[slug]/page.tsx
@@ -1,5 +1,14 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
 
 const DetailTrackPage = ({ params }: { params: { slug: string } }) => {
+
+    const searchParams = useSearchParams();
+
+    const search = searchParams.get('audio');
+
+    console.log('Search Params:', search);
+
     return (
         <div>Detail Track Page: {params.slug}</div>
     );
