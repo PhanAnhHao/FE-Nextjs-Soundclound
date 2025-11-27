41bdf8b88a787b92f29f96dfb09d9bc8344bef45
Anh Hào Phan <pdkenny25@gmail.com>
2025-11-27 15:59:15 +0700

    Module Wavesurfer: Fetch Track from Remote

--- nextjs-mui-ts-client/src/components/track/wave.track.tsx
+++ nextjs-mui-ts-client/src/components/track/wave.track.tsx
@@ -1,19 +1,22 @@
 'use client';
+import { useSearchParams } from "next/navigation";
 import { useEffect, useRef } from "react";
 import WaveSurfer from "wavesurfer.js";
 
 const WaveTrack = () => {
 
+    const searchParams = useSearchParams();
+    const fileName = searchParams.get('audio');
+
     const waveSurferRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        console.log("WaveSurfer ref:", waveSurferRef.current);
         if (waveSurferRef.current) {
             WaveSurfer.create({
                 container: waveSurferRef.current,
                 waveColor: 'rgba(107, 106, 107, 1)',
                 progressColor: 'rgba(230, 115, 15, 1)',
-                url: '/audio/CHILL.mp3',
+                url: `/api?audio=${fileName}`,
             });
         }
     }, []);
